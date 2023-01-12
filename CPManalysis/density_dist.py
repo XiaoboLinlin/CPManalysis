4a92ff0de5ad092a3e6bf995179d65b8f4324d2f
XiaoboLinlin <xiaobolin11@outlook.com>
2023-01-12 16:44:46 -0600

    add more functions to density file

--- CPManalysis/density_dist.py
+++ CPManalysis/density_dist.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def calc_density_distribution(trj_total, last_n_frame = 2500, res_name = 'tfsi', binwidth = 0.1):
+def calc_density_distribution(trj_total, last_n_frame = 2500, res_name = 'tfsi', binwidth = 0.1, axis = 2):
     """calculate com density distribution along one direction
 
     Args:
@@ -10,6 +10,7 @@ def calc_density_distribution(trj_total, last_n_frame = 2500, res_name = 'tfsi',
         last_n_frame (int or list): _description_. Defaults to 2500. Can select a range by using list type
         res_name (str, optional): _description_. Defaults to 'all'. It can be like 'emim
         binwidth (float, optional): _description_. Defaults to 0.1.
+        axis (int): analyze density along which axis. 2 is z axis, 0 is x axis, 1 is y axis
 
     Returns:
         new_bins, new_hist: _description_
@@ -28,19 +29,22 @@ def calc_density_distribution(trj_total, last_n_frame = 2500, res_name = 'tfsi',
     total_xyz = all_xyz
 
     total_xyz = np.reshape(total_xyz, (-1,3))
-    data = total_xyz[:,2]
+    data = total_xyz[:, axis]
     # binwidth = 0.1
     box = trj_total.unitcell_lengths[0]
-    bin_volumn = binwidth * box[0] * box[1]
+    
+    box_length = list(box[:])
+    box_length.remove(box[axis])
+    bin_volumn = binwidth * box_length[0] * box_length[1]
     n_frame = last_n_frame if type(last_n_frame) is int else len(last_n_frame)
     total_bin_volumn = bin_volumn * n_frame
     # avg_data = data/total_bin_volumn
 
-    gro_xyz = trj_total[-1].xyz[0]
-    gro_xyz = gro_xyz[:,2]
+    # gro_xyz = trj_total[-1].xyz[0]
+    # gro_xyz = gro_xyz[:,2]
     
     # bins = np.arange(min(gro_xyz), max(gro_xyz) + binwidth, binwidth)
-    bins = np.arange(0, box[2] + binwidth, binwidth)
+    bins = np.arange(0, box[axis] + binwidth, binwidth)
     
     hist, bin_edges = np.histogram(data, bins=bins)
 
